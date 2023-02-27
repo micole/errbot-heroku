30b950060b87ad3ea863df6f7da8603d2de8f23b
Michael Cole <micole.3@gmail.com>
2023-02-27 14:00:23 -0500

    testing on heroku

---
+++ config.py
@@ -0,0 +1,27 @@
+import logging
+import os
+
+# This is a minimal configuration to get you started with the Text mode.
+# If you want to connect Errbot to chat services, checkout
+# the options in the more complete config-template.py from here:
+# https://raw.githubusercontent.com/errbotio/errbot/master/errbot/config-template.py
+
+BACKEND = "SlackV3"  # Errbot will start in text mode (console only mode) and will answer commands from there.
+
+BOT_DATA_DIR = r"/app/errbot-heroku/data"
+BOT_EXTRA_PLUGIN_DIR = r"/app/errbot-heroku/plugins"
+
+BOT_LOG_FILE = r"/app/errbot-heroku/errbot.log"
+BOT_LOG_LEVEL = logging.INFO
+
+BOT_EXTRA_BACKEND_DIR = "/app/errbot-heroku/err-backend-slackv3"
+
+BOT_IDENTITY = {
+    'token': os.getenv('TOKEN'),
+    'signing_secret': os.getenv('SIGNING_SECRET'),
+    'app_token': os.getenv('APP_TOKEN')
+}
+
+BOT_ADMINS = (
+    "@micole.3",
+)  # Don't leave this as "@CHANGE_ME" if you connect your errbot to a chat system!!
